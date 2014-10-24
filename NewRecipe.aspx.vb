44a49f58a8dffbac2472de84506db60c372e51e0
Collin Petit <collin-petit@uiowa.edu>
2014-10-24 18:54:17 -0500

    resized detail recipe view

--- NewRecipe.aspx.vb
+++ NewRecipe.aspx.vb
@@ -5,4 +5,8 @@ Partial Class View
     Protected Sub newRecipeInsertView_ItemInserted(sender As Object, e As DetailsViewInsertedEventArgs) Handles newRecipeInsertView.ItemInserted
         Response.Redirect("Default.aspx")
     End Sub
+
+    Protected Sub Page_Load(sender As Object, e As EventArgs) Handles Me.Load
+        newRecipeInsertView.Focus() 'cursor change on load
+    End Sub
 End Class
